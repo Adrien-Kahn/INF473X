d8fece68a22793aa1cee5bc63ae35f1726bfee4c
VirtualAdrien <adrien.kahn.x19@polytechnique.edu>
2021-03-13 01:12:37 +0100

    King Crimson

---
+++ Hello_World/fancy-hello-world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "hello-world.h"
+
+int main(void) {
+	char c[] = "Hello World, hello ";
+	printf("%s", c);
+	return 0;
+}
+
+void hello_string(char* name, char* output) {
+	
+}
---
+++ Hello_World/international-hello-world.h
@@ -0,0 +1,4 @@
+#ifndef HELLO_WORLD_H
+#define HELLO_WORLD_H
+void print_international_hello_string();
+#endif
