9d9e14d08b4313d5f52318a5d850c8a071b5fdff
VirtualAdrien <adrien.kahn.x19@polytechnique.edu>
2021-04-23 18:17:45 +0200

    3A Done

--- 3A/multi_UDP_server.c
+++ 3A/multi_UDP_server.c
@@ -12,18 +12,19 @@
 void *process_message(void *);
 
 typedef struct Recv_info{
-	struct sockaddr_in from;
-	int *from_len;
+	int sock;
+	struct sockaddr_in *from;
 	int buf_len;
 	char *buf;
 } Recv_info;
 
 
 pthread_mutex_t writeMutex = PTHREAD_MUTEX_INITIALIZER;
+int active_threads = 0;
 
 
 int main(int argc, char* argv[]) {
-	
+
 	// Fetching port number
 	if (argc < 2) {
 		fprintf(stderr, "Missing argument, please enter IP address and port number\n");
@@ -47,64 +48,83 @@ int main(int argc, char* argv[]) {
 	
 	printf("Server started, waiting for incoming messages\n\n");
 
-	// Creating threads that run process_message
-	for (int k = 0; k < N_THREAD; k++) {
-		pthread_t thread;
-		if (pthread_create(&thread, NULL, process_message, &sockfd)) {
-			fprintf(stderr, "Failed to create thread\n");
-			return 1;
-		}
-	}
-
-	// Main threads does nothing
-	while (1) usleep(100000000);
-	
-}
-
-
-	
-void *process_message(void *sock_pointer) {
-	
+	// Main threads receives messages
 	while (1) {
-		
-		int sockfd = *((int *)sock_pointer);
-		
+	
 		// Building addr_in to identify the client
-		struct sockaddr_in from;
+		struct sockaddr_in *from = malloc(sizeof(struct sockaddr_in));
 		int *from_len;
 		int temp = (int) sizeof(struct sockaddr);
 		from_len = &temp;
 		
 		// Building received message buffer
 		int buf_len = 1000;
-		char buffer[buf_len];
-		char *buf = buffer;
+		char *buf = malloc(buf_len * sizeof(char));
 		memset(buf, 0, 1000);
 		
 		// Receiving message
-		int recv_len = recvfrom(sockfd, buf, buf_len, 0, (struct sockaddr*) &from, from_len);
+		int recv_len = recvfrom(sockfd, buf, buf_len, 0, (struct sockaddr*) from, from_len);
 		//printf("Length of the received message: %d\n", recv_len);
 		pthread_mutex_lock(&writeMutex);
 		printf("Received message: \n%s\n", buf);
 		pthread_mutex_unlock(&writeMutex);
 		
-		// Sleeping because receiving a message is tiring
-		usleep(10000000);
-		
-		// Sending it back
-		int sent_length = sendto(sockfd, buf, recv_len, 0, (struct sockaddr*) &from, sizeof(struct sockaddr));
-		if (sent_length != recv_len) {
-			pthread_mutex_lock(&writeMutex);
-			printf("Message not sent entirely\n\n");
-			pthread_mutex_unlock(&writeMutex);
+		// If we have not reached the maximum number of threads, we create a new one to send back the message
+		if (active_threads < N_THREAD) {
+			
+			Recv_info ri;
+			ri.sock = sockfd;
+			ri.from = from;
+			ri.buf_len = recv_len;
+			ri.buf = buf;
+			
+			active_threads++;			
+			pthread_t thread;
+			if (pthread_create(&thread, NULL, process_message, &ri)) {
+				fprintf(stderr, "Failed to create thread\n");
+				return 1;
+			}
+			
 		} else {
 			pthread_mutex_lock(&writeMutex);
-			printf("Sent back message:\n%s\n", buf);
+			printf("Message discarded: \n%s\n", buf);
 			pthread_mutex_unlock(&writeMutex);
+			
+			// Sending a message to the client saying we are too busy
+			char busy[] = "Sorry, we could not process your message, the server is too busy\n";
+			char *busy_buf = busy;
+			sendto(sockfd, busy_buf, strlen(busy_buf), 0, (struct sockaddr*) from, sizeof(struct sockaddr));
 		}
-		//printf("Sent length: %d\n", sent_length);
-		
+	
 	}
+	
+}
+
+
+	
+void *process_message(void *ptr) {
+	
+		
+	Recv_info ri = *((Recv_info *) ptr);
+	
+	// Sleeping because receiving a message is tiring
+	usleep(10000000);
+		
+	// Sending the message back
+	int sent_length = sendto(ri.sock, ri.buf, ri.buf_len, 0, (struct sockaddr*) ri.from, sizeof(struct sockaddr));
+	if (sent_length != ri.buf_len) {
+		pthread_mutex_lock(&writeMutex);
+		printf("Message not sent entirely\n\n");
+		pthread_mutex_unlock(&writeMutex);
+	} else {
+		pthread_mutex_lock(&writeMutex);
+		printf("Sent back message:\n%s\n", ri.buf);
+		pthread_mutex_unlock(&writeMutex);
+	}
+	//printf("Sent length: %d\n", sent_length);
+	
+	active_threads--;
+	free(ri.buf);
 	
 	return NULL;
 
